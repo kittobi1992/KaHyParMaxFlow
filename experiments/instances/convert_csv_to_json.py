8f84424f2e8b9aaa72af28565d25f23f6175c8b7
kittobi1992 <tobias.heuer@gmx.net>
2017-11-09 08:46:01 +0100

    create html page for benchmark sets

--- experiments/instances/convert_csv_to_json.py
+++ experiments/instances/convert_csv_to_json.py
@@ -1,11 +1,45 @@
 import csv
 import json
 
+############## Convert Full Benchmark Set to JSON ##############
+
 csvfile = open('benchmark_set_stats.csv', 'r')
 jsonfile = open('benchmark_set_stats.json', 'w')
 
 fieldnames = ["graph","HNs","HEs","pins","avgHEsize","sdHEsize","minHEsize","heSize90thPercentile","Q1HEsize","medHEsize","Q3HEsize","maxHEsize",
               "avgHNdegree", "sdHNdegree","minHNdegree","hnDegree90thPercentile","maxHNdegree","Q1HNdegree","medHNdegree","Q3HNdegree","density"]
 reader = csv.DictReader( csvfile, fieldnames)
-out = json.dumps( [ row for row in reader ] )
+
+full_benchmark_set = []
+for row in reader:
+    for name in fieldnames:
+        if name != "graph":
+            row[name] = float(row[name])
+    full_benchmark_set = full_benchmark_set + [row]
+out = json.dumps( full_benchmark_set )
 jsonfile.write(out)
+jsonfile.close()
+
+############## Convert Benchmark Subset to JSON ##############
+
+subsetfile = open('benchmark_subset.txt', 'r')
+jsonfile = open('benchmark_subset_stats.json', 'w')
+
+subset = set([x.strip() for x in subsetfile.readlines()])
+benchmark_subset = [x for x in full_benchmark_set if x['graph'] in subset]
+
+out = json.dumps( benchmark_subset )
+jsonfile.write(out)
+jsonfile.close()
+
+############## Convert Parameter Tunning Benchmark Set to JSON ##############
+
+parameterfile = open('parameter_tuning_subset.txt', 'r')
+jsonfile = open('parameter_tuning_subset_stats.json', 'w')
+
+subset = set([x.strip() for x in parameterfile.readlines()])
+parameter_subset = [x for x in full_benchmark_set if x['graph'] in subset]
+
+out = json.dumps( parameter_subset )
+jsonfile.write(out)
+jsonfile.close()
